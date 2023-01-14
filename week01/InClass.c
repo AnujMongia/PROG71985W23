3f4d61c8d7f7fe3c6c1bc6bafe35f975a9e11cca
professor steve h <steve@use.my.school.email.addr>
2023-01-13 19:42:04 -0500

    week01 class work and week02 prep

---
+++ week01/InClass.c
@@ -0,0 +1,22 @@
+// program to take input, convert from inches to cm, and print to screen
+
+// steveh - prog71985 - winter 2023 - assignment #
+
+void main(void)
+{
+
+
+// user input
+
+
+//convert inches to cm
+    float cmOutput = 2.54 * inchesInput;
+
+// output
+//float  (single precision)
+    printf("convert from %f inches is %f cm\n", inchesInput, cmOutput);
+
+
+
+
+}
